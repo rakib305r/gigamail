f729fa255c11cac1c90ebe7811027df898125e6d
rakib305r <rakibofficial305r@gmail.com>
2025-07-26 15:25:12 +0600

    Update circle.java

--- circle.java
+++ circle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class circle{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-
+//
      double R;
      double circumference;
      double area; 
@@ -16,7 +16,7 @@ public class circle{
      volume = (4/3)*Math.PI*Math.pow(R, 3);
 
      System.out.println("Circle Area is: "+ area + "cm²");
-     System.out.println("The circumference is: ", circumference+ "cm²");
+     System.out.println("The circumference is: "+ circumference+ "cm²");
      System.out.println("Volume is: "+ volume+"cm³");
 
      scanner.close();
