03f746f8601981f85b6c29225066215b847eb702
rakib305r <rakibofficial305r@gmail.com>
2025-07-27 08:31:15 +0600

    UPDATE

--- circle.py
+++
@@ -1,6 +0,0 @@
-import math
-a=3.1416
-r=float(input("Enter of value of r:"))
-Area= (a*r*r)
-print(Area)
-cir():
--- reverse.py
+++
@@ -1,7 +0,0 @@
-a= input("Enter Anything:")
-b= a.split()
-
-c= ""
-for i in range(len(b)-1, -1, -1):
-    c += b[i]+ " "
-print(c)
