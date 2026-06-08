1e36df5354183ff1afcae68acfad5be22c14c975
Rakib Ahmed <rakibofficial305r@gmail.com>
2026-06-08 16:08:48 +0600

    UPDATE

---
+++ JAVA/inheritence.java
@@ -0,0 +1,44 @@
+class Student {
+    String Name;
+    int Roll;
+
+    void GetData() {
+        Name = "Rakib Ahmedd";
+        Roll = 871203;
+    }
+
+    void Display() {
+        System.out.println("Roll is: " + Roll);
+        System.out.println("Name is: " + Name);
+    }
+}
+
+class Result extends Student {
+    float Mark;
+    @Override
+    void GetData() {
+        super.GetData();
+        Mark = 85.5f;
+    }
+
+    @Override
+    void Display() {
+        super.Display();
+        System.out.println("Mark is: " + Mark);
+    }
+}
+
+class exam extends Result {
+    @Override
+    void Display() {
+        super.Display();
+    }
+}
+
+public class inheritence {
+    public static void main(String[] args) {
+        Result result = new Result();
+        result.GetData();
+        result.Display();
+    }
+}
