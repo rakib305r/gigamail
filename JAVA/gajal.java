87a9e541f5b45a9403036c05155134326fb198b8
rakib305r <rakibofficial305r@gmail.com>
2026-07-03 00:05:34 +0600

    gajal update

--- JAVA/gajal.java
+++ JAVA/gajal.java
@@ -1,34 +1,87 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class gajal {
 
-    public static void main(String[] args) throws InterruptedException {
+    static String[][] lyrics = {
+            {"Hamd-o-Sana Rabb-e-Zul-Jalal Ki", "CYAN"},
+            {"", "WHITE"},
+            {"Kardo Karam Maula, Kardo Karam", "YELLOW"},
+            {"Hum Pe Karam Maula, Hum Pe Karam", "BLUE"},
+            {"Kardo Karam Maula, Kardo Karam", "YELLOW"},
+            {"Hum Pe Karam Allah, Hum Pe Karam", "BLUE"},
+            {"", "WHITE"},
+            {"Bikhre Huye Sharmsaar Hain Hum", "MAGENTA"},
+            {"Rehmat Ke Talabgaar Hain Hum", "RED"},
+            {"Wasta Nabi Ka Maula Rakh Lo Bharam", "CYAN"},
+            {"", "WHITE"},
+            {"Kardo Karam Maula, Kardo Karam", "YELLOW"},
+            {"Hum Pe Karam Maula, Hum Pe Karam", "BLUE"},
+            {"Kardo Karam Allah, Kardo Karam", "GREEN"},
+            {"Hum Pe Karam Maula, Hum Pe Karam", "MAGENTA"}
+    };
 
-        String[] lyrics = {
+    static int index = 0;
 
-            "Bismillah",
-            "",
-            "Hamd-o-Sana Rabb-e-Zul-Jalal Ki",
-            "",
-            "Kardo Karam Maula, Kardo Karam",
-            "Hum Pe Karam Maula, Hum Pe Karam",
-            "Kardo Karam Maula, Kardo Karam",
-            "Hum Pe Karam Allah, Hum Pe Karam",
-            "",
-            "Bikhre Huye Sharmsaar Hain Hum",
-            "Rehmat Ke Talabgaar Hain Hum",
-            "Wasta Nabi Ka Maula Rakh Lo Bharam",
-            "",
-            "Kardo Karam Maula, Kardo Karam",
-            "Hum Pe Karam Maula, Hum Pe Karam",
-            "Kardo Karam Allah, Kardo Karam",
-            "Hum Pe Karam Maula, Hum Pe Karam",
-        };
+    public static void main(String[] args) {
 
-        int delay = 2500;
-        for (String line : lyrics) {
-            System.out.println(line);
-            Thread.sleep(delay);
-        }
+        JFrame frame = new JFrame("🕌 Kardo Karam");
+        frame.setSize(800, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
 
-        System.out.println("\n--- End of Lyrics ---");
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(15, 23, 42)); // Dark Blue
+        panel.setLayout(new BorderLayout());
+
+        JLabel title = new JLabel("🕌 Kardo Karam", SwingConstants.CENTER);
+        title.setFont(new Font("Serif", Font.BOLD, 30));
+        title.setForeground(Color.ORANGE);
+
+        JLabel lyric = new JLabel("", SwingConstants.CENTER);
+        lyric.setFont(new Font("Arial", Font.BOLD, 28));
+
+        panel.add(title, BorderLayout.NORTH);
+        panel.add(lyric, BorderLayout.CENTER);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        Timer timer = new Timer(2500, e -> {
+
+            lyric.setText(lyrics[index][0]);
+
+            switch (lyrics[index][1]) {
+                case "GREEN":
+                    lyric.setForeground(Color.GREEN);
+                    break;
+                case "CYAN":
+                    lyric.setForeground(Color.CYAN);
+                    break;
+                case "YELLOW":
+                    lyric.setForeground(Color.YELLOW);
+                    break;
+                case "BLUE":
+                    lyric.setForeground(Color.BLUE);
+                    break;
+                case "MAGENTA":
+                    lyric.setForeground(Color.MAGENTA);
+                    break;
+                case "RED":
+                    lyric.setForeground(Color.RED);
+                    break;
+                default:
+                    lyric.setForeground(Color.WHITE);
+            }
+
+            index++;
+
+            if (index == lyrics.length) {
+                index = 0;
+            }
+
+        });
+
+        timer.start();
     }
 }
